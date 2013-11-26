9391806b0cfb616512be84c009cac1b4e189427e
Josh Barr <josh@springload.co.nz>
2013-11-26 17:51:23 +1300

    added build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,11 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+
+        fred_objects: ["scss/objects/_*.scss"],
+
         concat: {
-            sass: {
+            fred: {
                 src: [
                     "scss/_variables.scss",
                     "scss/helpers/*.scss",
@@ -17,7 +20,6 @@ module.exports = function(grunt) {
                     "scss/objects/_forms.scss",
                     "scss/objects/_headings.scss",
                     "scss/objects/_icon-text.scss",
-
                     "scss/objects/_images.scss",
                     "scss/objects/_lists.scss",
                     "scss/objects/_media.scss",
@@ -35,6 +37,14 @@ module.exports = function(grunt) {
                 files: {
                     'dist/fred.css': 'scss/fred.scss'
                 }
+            },
+            build: {
+                options: {
+                    outputStyle: 'nested'
+                },
+                files: {
+                    'dist/fred.css': 'dist/fred.scss'
+                }
             }
         },
         watch: {
@@ -45,12 +55,37 @@ module.exports = function(grunt) {
                     spawn: false
                 }
             }
+        },
+        prompt: {
+            build: {
+                options: {
+                    questions: [
+                        {
+                            config: 'concat.fred.src', // arbitray name or config for any other grunt task
+                            type: 'checkbox', // list, checkbox, confirm, input, password
+                            message: 'Which modules would you like to include?',
+                            default: 'value', // default value if nothing is entered
+                            choices: function(answers) {
+                                var answers = grunt.file.expand(["scss/objects/_*.scss"]);
+                                console.log(answers);
+                                return answers;
+                            }
+//                            validate: function(value), // return true if valid, error message if invalid
+//                            filter:  function(value), // modify the answer
+//                            when: function(answers) // only ask this question when this function returns true
+                        }
+                    ]
+                }
+            },
         }
     });
 
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['sass', 'concat:sass']);
+    grunt.loadNpmTasks('grunt-prompt');
+    grunt.registerTask('default', ['sass', 'concat:fred']);
+
+    grunt.registerTask('build', ["prompt:build", "concat:fred", "sass:build"]);
 
 };
