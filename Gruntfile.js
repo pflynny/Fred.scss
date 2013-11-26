1daa663f63cfd00ab0c6bc69c9d7d1fe37443464
Josh Barr <josh@Joshs-MacBook-Pro-2.local>
2013-11-27 01:39:39 +1300

    added copy tasks to prompt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,21 @@
 module.exports = function(grunt) {
 
-    var fred_core = ["scss/_variables.scss", "scss/helpers/*.scss", "scss/objects/_core.scss"];
+    var fred_core = ["scss/helpers/*.scss", "scss/objects/_core.scss"];
     var objects = ["scss/objects/*.scss", "!scss/objects/_core.scss"];
 
     grunt.initConfig({
 
         fred_objects: ["scss/objects/_*.scss"],
-        custom: false,
+        fred_variables: "_fred-variables.scss",
 
         concat: {
             core: {
-                src: ["scss/_variables.scss", "scss/helpers/*.scss", "scss/objects/_core.scss"],
+                src: ["scss/helpers/*.scss", "scss/objects/_core.scss"],
                 dest: "build/fred.core.scss"
+            },
+            vars: {
+                src: ["scss/_variables.scss"],
+                dest: "build/_fred-variables.scss"
             },
             objects: {
                 src: objects,
@@ -20,10 +24,33 @@ module.exports = function(grunt) {
             dist: {
                 src: ["build/fred.core.scss", "build/fred.objects.scss"],
                 dest: "dist/fred.scss"
+            },
+            build: {
+                src: ["build/_fred-variables.scss", "dist/fred.scss"],
+                dest: "build/fred.build.scss"
             }
         },
         clean: {
-            dist: ["build"]
+            build: ["build"]
+        },
+        copy: {
+            build: {
+                src: "build/_fred-variables.scss",
+                dest: "dist/_fred-variables.scss"
+            },
+            install: {
+                // files: [{
+                    // expand: true,
+                    // cwd: "dist/",
+                    src: "dist/fred.scss",
+                    dest: "<%= copy.install.thing %>/lib/_fred.scss"
+                // }]    
+            },
+            vars: {
+                src: "dist/_fred-variables.scss",
+                dest: "<%= copy.install.thing %>/_fred-variables.scss"
+            }
+
         },
         sass: {
             dist: {
@@ -39,7 +66,7 @@ module.exports = function(grunt) {
                     outputStyle: 'nested'
                 },
                 files: {
-                    'dist/fred.css': 'dist/fred.scss'
+                    'dist/fred.css': 'build/fred.build.scss'
                 }
             }
         },
@@ -79,17 +106,74 @@ module.exports = function(grunt) {
                     ]
                 }
             },
+            install: {
+                options: {
+                    questions: [
+                        {
+                            config: 'install_now', // arbitray name or config for any other grunt task
+                            type: 'confirm', // list, checkbox, confirm, input, password
+                            message: 'Do you want to install Fred into your project now?',
+                            default: true // default value if nothing is entered
+                        },
+                        {
+                            config: 'copy.install.thing', // arbitray name or config for any other grunt task
+                            type: 'text', // list, checkbox, confirm, input, password
+                            message: 'Where would you like Fred deployed to?',
+                            default: "www/assets/sass", // default value if nothing is entered
+                            when: function(answers) {
+                                return answers['install_now'] === true;
+                            },
+                        },
+                        {
+                            config: 'copy.install.vars', // arbitray name or config for any other grunt task
+                            type: 'confirm', // list, checkbox, confirm, input, password
+                            message: 'Copy the variables file too (will replace your current variables)?',
+                            default: false // default value if nothing is entered
+                        },
+
+                    ]
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-prompt');
 
+
+    grunt.registerTask("installer", function() {
+        var dest = grunt.config('copy.install.thing');
+        var vars = grunt.config('copy.install.vars');
+
+            if (dest) {
+                grunt.task.run('copy:install');
+                if (vars) {
+                    grunt.task.run('copy:vars');
+                }
+            } else {
+                return true;
+            }
+
+    });
+
     // grunt.registerTask('default', ['sass', 'concat:core', "concat:objects", "concat:dist", "clean:dist"]);
 
-    grunt.registerTask('default', ["prompt:build", 'concat:core', "concat:objects", "concat:dist", "clean:dist", "sass:build"]);
+    grunt.registerTask('default', [
+        "prompt:build",
+        'concat:vars',
+        'concat:core',
+        "concat:objects",
+        "concat:dist",
+        "concat:build",
+        "copy:build",
+        "sass:build",
+        "clean:build",
+        "prompt:install",
+        "installer"
+        ]);
 
 };
--- README.md
+++ README.md
@@ -2,12 +2,38 @@
 
 Fred is a collection of common CSS patterns for websites.
 
-You can build a custom package by running `grunt <module-name> <other-module-name>`. Defaults to all.
+## Building Fred
+ 
+To build a custom Fred package from the command line, run:
 
-Grunt will make a `dist/fred.scss`, and compiled `dist/fred.css` for your project.
+```
+> npm install
+> grunt
+```
+
+...and follow the directions of your cabin crew.
+
+Grunt will make a `dist/fred.scss`, and compile `dist/fred.css` for your project
 
 
+## Working with your project
 
-## Installation
+You want to tweak all Fred's variables and make everything pretty for your project.
 
-Run `npm install` to set up all Fred's dependencies
+You also want to keep Fred under some kind of package management, like bower. 
+
+Copy `_fred-variables.scss` into your sass directory before you include `fred.scss`.
+
+
+```sass
+// style.scss
+
+@import "fred-variables";
+@import "path/to/fred";
+
+// Rest of your styles here!
+
+```
+
+Now you can tweak away, while still being able to update Fred without nuking all your hard work.
+
